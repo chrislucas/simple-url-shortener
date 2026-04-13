6c862a1223220f4ffda418e49cb79859981198b6
chrislucas <christoffer.luccas@gmail.com>
2026-04-13 09:22:48 -0300

    corrigindo testes unitarios que não condiziam mais com as classes que foram refatoradas

--- URLShortener/app/src/test/java/com/br/urlshortener/HttpClientTest.kt
+++ URLShortener/app/src/test/java/com/br/urlshortener/HttpClientTest.kt
@@ -11,6 +11,9 @@ import org.junit.Before
 import org.junit.Test
 import retrofit2.Converter
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import kotlin.reflect.KClass
 
 class HttpClientTest {
 
@@ -21,7 +24,6 @@ class HttpClientTest {
         unmockkAll()
         mockkStatic(Log::class)
         every { Log.isLoggable(any(), any()) } returns false
-        every { Log.println(any(), any(), any()) } returns 0
     }
 
     @After
@@ -30,111 +32,137 @@ class HttpClientTest {
     }
 
     @Test
-    fun `createService should build retrofit and create service`() {
-        mockkConstructor(Retrofit.Builder::class)
-        val mockBuilder = mockk<Retrofit.Builder>(relaxed = true)
-        val mockRetrofit = mockk<Retrofit>(relaxed = true)
-        val mockService = mockk<DummyService>()
+    fun `Builder should configure all fields correctly`() {
+        val url = "https://api.example.com/"
+        val converterFactory = GsonConverterFactory.create()
+        val isDebug = true
+        val connectTimeout = 15L
+        val readTimeout = 20L
 
-        every { mockBuilder.baseUrl(any<String>()) } returns mockBuilder
-        every { mockBuilder.client(any<OkHttpClient>()) } returns mockBuilder
-        every { mockBuilder.addConverterFactory(any<Converter.Factory>()) } returns mockBuilder
-        every { mockBuilder.build() } returns mockRetrofit
-        every { mockRetrofit.create(DummyService::class.java) } returns mockService
+        val httpClient = HttpClient.Builder(url)
+            .withConverterFactory(converterFactory)
+            .isDebugMode(isDebug)
+            .withConnectionTimeout(connectTimeout)
+            .withReadTimeout(readTimeout)
+            .build()
 
-        every { anyConstructed<Retrofit.Builder>().baseUrl(any<String>()) } returns mockBuilder
-        every { anyConstructed<Retrofit.Builder>().client(any<OkHttpClient>()) } returns mockBuilder
-        every { anyConstructed<Retrofit.Builder>().addConverterFactory(any<Converter.Factory>()) } returns mockBuilder
-        every { anyConstructed<Retrofit.Builder>().build() } returns mockRetrofit
-
-        val service = HttpClient.createService<DummyService>(
-            url = "http://example.com/",
-            isDebug = false,
-        )
-
-        assertSame(mockService, service)
-        verify { anyConstructed<Retrofit.Builder>().baseUrl("http://example.com/") }
-        verify { anyConstructed<Retrofit.Builder>().build() }
-        verify { mockRetrofit.create(DummyService::class.java) }
+        assertNotNull(httpClient)
     }
 
     @Test
-    fun `createService should add logging interceptor when debug true`() {
+    fun `createService should build Retrofit with correct parameters`() {
+        mockkConstructor(Retrofit.Builder::class)
+        mockkConstructor(OkHttpClient.Builder::class)
+        
+        val mockRetrofitBuilder = mockk<Retrofit.Builder>(relaxed = true)
+        val mockOkBuilder = mockk<OkHttpClient.Builder>(relaxed = true)
+        
+        every { anyConstructed<OkHttpClient.Builder>().connectTimeout(any(), any()) } answers {
+            mockOkBuilder.connectTimeout(arg(0), arg(1))
+            mockOkBuilder
+        }
+        every { anyConstructed<OkHttpClient.Builder>().readTimeout(any(), any()) } answers {
+            mockOkBuilder.readTimeout(arg(0), arg(1))
+            mockOkBuilder
+        }
+        every { anyConstructed<OkHttpClient.Builder>().build() } answers {
+            mockOkBuilder.build()
+        }
+        
+        every { anyConstructed<Retrofit.Builder>().baseUrl(any<String>()) } answers {
+            mockRetrofitBuilder.baseUrl(it.invocation.args[0] as String)
+            mockRetrofitBuilder
+        }
+        every { anyConstructed<Retrofit.Builder>().client(any()) } answers {
+            mockRetrofitBuilder.client(it.invocation.args[0] as OkHttpClient)
+            mockRetrofitBuilder
+        }
+        every { anyConstructed<Retrofit.Builder>().addConverterFactory(any()) } answers {
+            mockRetrofitBuilder.addConverterFactory(it.invocation.args[0] as Converter.Factory)
+            mockRetrofitBuilder
+        }
+        every { anyConstructed<Retrofit.Builder>().build() } answers {
+            mockRetrofitBuilder.build()
+        }
+
+        val mockRetrofit = mockk<Retrofit>()
+        val mockOkClient = mockk<OkHttpClient>()
+        val mockService = mockk<DummyService>()
+
+        every { mockOkBuilder.build() } returns mockOkClient
+        every { mockRetrofitBuilder.build() } returns mockRetrofit
+        every { mockRetrofit.create(any<Class<DummyService>>()) } returns mockService
+
+        val url = "https://api.test.com/"
+        val httpClient = HttpClient.Builder(url)
+            .withConnectionTimeout(10L)
+            .withReadTimeout(20L)
+            .isDebugMode(false)
+            .build()
+
+        httpClient.createService(DummyService::class)
+
+        verify { mockOkBuilder.connectTimeout(10L, TimeUnit.SECONDS) }
+        verify { mockOkBuilder.readTimeout(20L, TimeUnit.SECONDS) }
+        verify { mockRetrofitBuilder.baseUrl(url) }
+        verify { mockRetrofitBuilder.client(mockOkClient) }
+    }
+
+    @Test
+    fun `createService should add LoggingInterceptor when isDebug is true`() {
         mockkConstructor(OkHttpClient.Builder::class)
         mockkConstructor(HttpLoggingInterceptor::class)
-        val mockOkBuilder = mockk<OkHttpClient.Builder>(relaxed = true)
-        val mockOkClient = mockk<OkHttpClient>(relaxed = true)
-
-        every { mockOkBuilder.addInterceptor(any<Interceptor>()) } returns mockOkBuilder
-        every { mockOkBuilder.build() } returns mockOkClient
-
-        every { anyConstructed<OkHttpClient.Builder>().addInterceptor(any<Interceptor>()) } returns mockOkBuilder
-        every { anyConstructed<OkHttpClient.Builder>().build() } returns mockOkClient
-
-        every {
-            anyConstructed<HttpLoggingInterceptor>().setLevel(HttpLoggingInterceptor.Level.BODY)
-        } returns mockk()
-
-        // Retrofit builder mocked to complete the flow and return a service
         mockkConstructor(Retrofit.Builder::class)
+
+        val mockOkBuilder = mockk<OkHttpClient.Builder>(relaxed = true)
+        val mockLogging = mockk<HttpLoggingInterceptor>(relaxed = true)
+        
+        every { anyConstructed<OkHttpClient.Builder>().addInterceptor(any<Interceptor>()) } answers {
+            mockOkBuilder.addInterceptor(it.invocation.args[0] as Interceptor)
+            mockOkBuilder
+        }
+        every { anyConstructed<HttpLoggingInterceptor>().setLevel(any()) } answers {
+            mockLogging.setLevel(it.invocation.args[0] as HttpLoggingInterceptor.Level)
+            mockLogging
+        }
+        
+        // Mocking Retrofit to avoid crashes
         val mockRetrofitBuilder = mockk<Retrofit.Builder>(relaxed = true)
-        val mockRetrofit = mockk<Retrofit>(relaxed = true)
-        val mockService = mockk<DummyService>()
-        every { mockRetrofitBuilder.baseUrl(any<String>()) } returns mockRetrofitBuilder
-        every { mockRetrofitBuilder.client(any<OkHttpClient>()) } returns mockRetrofitBuilder
-        every { mockRetrofitBuilder.addConverterFactory(any<Converter.Factory>()) } returns mockRetrofitBuilder
-        every { mockRetrofitBuilder.build() } returns mockRetrofit
-        every { mockRetrofit.create(DummyService::class.java) } returns mockService
-
         every { anyConstructed<Retrofit.Builder>().baseUrl(any<String>()) } returns mockRetrofitBuilder
-        every { anyConstructed<Retrofit.Builder>().client(any<OkHttpClient>()) } returns mockRetrofitBuilder
-        every { anyConstructed<Retrofit.Builder>().addConverterFactory(any<Converter.Factory>()) } returns mockRetrofitBuilder
-        every { anyConstructed<Retrofit.Builder>().build() } returns mockRetrofit
 
-        val service = HttpClient.createService<DummyService>(
-            url = "http://example.com/",
-            isDebug = true,
-        )
-        assertSame(mockService, service)
+        val httpClient = HttpClient.Builder("https://test.com/")
+            .isDebugMode(true)
+            .build()
 
-        verify { anyConstructed<HttpLoggingInterceptor>().setLevel(HttpLoggingInterceptor.Level.BODY) }
-        verify { anyConstructed<OkHttpClient.Builder>().addInterceptor(ofType<HttpLoggingInterceptor>()) }
+        httpClient.createService(DummyService::class)
+
+        verify { mockOkBuilder.addInterceptor(any<HttpLoggingInterceptor>()) }
+        verify { mockLogging.setLevel(HttpLoggingInterceptor.Level.BODY) }
     }
 
     @Test
-    fun `createService should not add logging interceptor when debug false`() {
+    fun `createService should NOT add LoggingInterceptor when isDebug is false`() {
         mockkConstructor(OkHttpClient.Builder::class)
-        val mockOkBuilder = mockk<OkHttpClient.Builder>(relaxed = true)
-        val mockOkClient = mockk<OkHttpClient>(relaxed = true)
-
-        every { mockOkBuilder.addInterceptor(any<Interceptor>()) } returns mockOkBuilder
-        every { mockOkBuilder.build() } returns mockOkClient
-
-        every { anyConstructed<OkHttpClient.Builder>().addInterceptor(any<Interceptor>()) } returns mockOkBuilder
-        every { anyConstructed<OkHttpClient.Builder>().build() } returns mockOkClient
-
-        // Retrofit builder mocked to complete the flow and return a service
         mockkConstructor(Retrofit.Builder::class)
+
+        val mockOkBuilder = mockk<OkHttpClient.Builder>(relaxed = true)
+        
+        every { anyConstructed<OkHttpClient.Builder>().connectTimeout(any(), any()) } returns mockOkBuilder
+        
+        every { mockOkBuilder.connectTimeout(any(), any()) } returns mockOkBuilder
+        every { mockOkBuilder.readTimeout(any(), any()) } returns mockOkBuilder
+        every { mockOkBuilder.addInterceptor(any<Interceptor>()) } returns mockOkBuilder
+        every { mockOkBuilder.build() } returns mockk(relaxed = true)
+        
         val mockRetrofitBuilder = mockk<Retrofit.Builder>(relaxed = true)
-        val mockRetrofit = mockk<Retrofit>(relaxed = true)
-        val mockService = mockk<DummyService>()
-        every { mockRetrofitBuilder.baseUrl(any<String>()) } returns mockRetrofitBuilder
-        every { mockRetrofitBuilder.client(any<OkHttpClient>()) } returns mockRetrofitBuilder
-        every { mockRetrofitBuilder.addConverterFactory(any<Converter.Factory>()) } returns mockRetrofitBuilder
-        every { mockRetrofitBuilder.build() } returns mockRetrofit
-        every { mockRetrofit.create(DummyService::class.java) } returns mockService
-
         every { anyConstructed<Retrofit.Builder>().baseUrl(any<String>()) } returns mockRetrofitBuilder
-        every { anyConstructed<Retrofit.Builder>().client(any<OkHttpClient>()) } returns mockRetrofitBuilder
-        every { anyConstructed<Retrofit.Builder>().addConverterFactory(any<Converter.Factory>()) } returns mockRetrofitBuilder
-        every { anyConstructed<Retrofit.Builder>().build() } returns mockRetrofit
 
-        val service = HttpClient.createService<DummyService>(
-            url = "http://example.com/",
-            isDebug = false,
-        )
-        assertSame(mockService, service)
+        val httpClient = HttpClient.Builder("https://test.com/")
+            .isDebugMode(false)
+            .build()
 
-        verify(exactly = 0) { anyConstructed<OkHttpClient.Builder>().addInterceptor(any<Interceptor>()) }
+        httpClient.createService(DummyService::class)
+
+        verify(exactly = 0) { mockOkBuilder.addInterceptor(any<Interceptor>()) }
     }
 }
